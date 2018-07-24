095d98473efa357e5b8d23f440cf7046afbba793
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-07-24 00:49:39 -0400

    little header thing

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,17 @@
 import { h, Component } from 'preact';
 
 import './app.css';
+import SmallCaps from './SmallCaps';
 import Stats from './Stats';
 import Debug from './Debug';
 
 export default function App({ }, { }) {
 	return (
 		<div class="app">
+			<header>
+				<h1><SmallCaps>The Lady's Book Of Decency</SmallCaps></h1>
+				<h2>A Practical Treatise on Manners, Feeding, and Etiquette</h2>
+			</header>
 				<Stats />
 				<Debug />
 		</div>
