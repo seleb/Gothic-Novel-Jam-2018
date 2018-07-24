6d601aec110309fba7d77a2355b259150449802d
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-07-24 02:01:53 -0400

    use devtools + prepare for middleware

--- src/store.js
+++ src/store.js
@@ -1,9 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+
 import reducers from './reducers';
+import middleware from './middleware';
 
 const initialState = {};
-export default createStore(
+
+const store = createStore(
 	reducers,
 	initialState,
-	typeof devToolsExtension === 'function' ? devToolsExtension() : undefined
+	composeWithDevTools(applyMiddleware(...middleware)),
 );
+
+export default store;
