23ad9a4fd5b3022ac509ba3dd604e295f4f56210
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2018-07-25 01:47:47 -0400

    polish animation
    fixes issue where text can be seen writing to the end of a line, and then wraps as it hits the edge with new letters by including the remainder of the last word invisibly

--- src/components/strand/TextNode.jsx
+++ src/components/strand/TextNode.jsx
@@ -38,12 +38,38 @@ export class TextNode extends Component {
 		if (shown < 0) {
 			return null;
 		}
+		const { length = 0 } = content;
+		if (shown >= length) {
+			// animation complete
+			return <span className="text-node">{content}</span>;
+		}
+
+		// mid animation, split into various subsections
+		// all shown characters
 		const shownChars = content.substr(0, shown);
-		const nextChar = content.substr(shown, 1);
+		// animated character (next being shown)
+		const animChar = content.substr(shown, 1);
+		// split the shown characters into fully shown words,
+		// and last partial word
+		// if the animated character is a space,
+		// then there is no last partial word
+		const {
+			1: shownWords,
+			2: lastWord,
+		} = animChar
+			.match(/\s/)
+				? [undefined, shownChars, undefined]
+				: shownChars.split(/(.*\s)?(.*)/);
+		// remainder of partial word (if there is one)
+		const hiddenChars = content.substr(shown + 1).split(' ')[0];
 		return (
 			<span className="text-node">
-				{shownChars}
-				{nextChar && <span onAnimationEnd={this.tick} className={` last last-${shown % 2}`}>{nextChar}</span>}
+				{shownWords}
+				<span className="last-word">
+					{lastWord}
+					{animChar && <span onAnimationEnd={this.tick} className={` last last-${shown % 2}`}>{animChar}</span>}
+					{hiddenChars && <span className="break-guard">{hiddenChars}</span>}
+				</span>
 			</span>
 		);
 	}
