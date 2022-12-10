cbd5550df07f3eee3766a4cc1a77314297cef2ab
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2022-12-09 23:44:31 -0500

    fix misaligned quotes

--- src/components/strand/Node.jsx
+++ src/components/strand/Node.jsx
@@ -4,6 +4,24 @@ import { h, Component } from 'preact';
 import TextNode from './TextNode';
 import ActionNode from './ActionNode';
 
+/**
+ * replaces regular quotes with context-aware smart quotes
+ * @param {string} str string to replace
+ */
+function smartify(str = '') {
+	return str
+		.replace(
+			/("+)(.*?)("+)/g,
+			(_, l, i, r) => `${'“'.repeat(l.length)}${i}${'”'.repeat(r.length)}`
+		)
+		.replace(/(\w)'(\w)/g, '$1’$2')
+		.replace(
+			/('+)(.*?)('+)/g,
+			(_, l, i, r) => `${'‘'.repeat(l.length)}${i}${'’'.repeat(r.length)}`
+		);
+}
+
+
 export default function Node({
 	name = '',
 	value,
@@ -11,7 +29,7 @@ export default function Node({
 }) {
 	switch (name) {
 		case 'text':
-			return <TextNode idx={idx}>{value}</TextNode>;
+			return <TextNode idx={idx}>{smartify(value)}</TextNode>;
 		case 'action':
 			return <ActionNode {...value} idx={idx} />
 		default:
